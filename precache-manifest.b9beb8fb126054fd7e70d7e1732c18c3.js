690f899ca16c8aa267551f34be75cadceee3f9a2
Ashish-29 <ashishk6285@gmail.com>
2022-06-03 17:19:16 +0530

    Updates

---
+++ precache-manifest.b9beb8fb126054fd7e70d7e1732c18c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b77a6cfaaaf070a022e6",
+    "url": "/TextUtils-React/static/css/main.6238b76b.chunk.css"
+  },
+  {
+    "revision": "b77a6cfaaaf070a022e6",
+    "url": "/TextUtils-React/static/js/main.b77a6cfa.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils-React/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "9f467190b4df4fe2318e",
+    "url": "/TextUtils-React/static/js/2.9f467190.chunk.js"
+  },
+  {
+    "revision": "54d4ef26c5198954da45",
+    "url": "/TextUtils-React/static/js/runtime~main.54d4ef26.js"
+  },
+  {
+    "revision": "3cfa6d8d31c262a31fb48b3facfd4455",
+    "url": "/TextUtils-React/index.html"
+  }
+];
